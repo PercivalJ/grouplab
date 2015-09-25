e12c260d52e5aa6de3f7cc147dc36085cfb2eee4
Greg Pynes Pynes <gpynes@users.noreply.github.com>
2015-09-25 14:14:59 -0700

    Adds User Routes and Model

--- models/user.js
+++ models/user.js
@@ -1,13 +1,38 @@
 //USER MODEL
 //==========
 
+
 //DECLARING VARIABLES
 //===================
+var mongoose = require( 'mongoose' );
+var bcrypt   = require( 'bcrypt-nodejs' );
+var Schema   = mongoose.Schema;
 
 //USER SCHEMA
 //===========
+var UserSchema = new Schema( {
+	name: String,
+	email: { type: String, required: true, index: { unique: true } },
+	password: { type: String, required: true, select: false }
+} );
 
-
+//USER ACTIONS BEFORE SAVE
+//========================
+UserSchema.pre( 'save', function( next ) {
+	var user = this;
+	if ( !user.isModified( 'password' ) ) {
+		return next();
+	}
+	
+	bcrypt.hash( user.password, null, null, function( err, hash ) {
+		if ( err ) {
+			return next( err );
+		}
+		user.password = hash;
+		next();
+	} );
+} );
 
 //EXPORT SCHEMA
 //=============
+module.exports = mongoose.model( 'User', UserSchema );
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,23 @@
 
 //DECLARING VARIABLES
 //===================
-
+var express         = require( 'express' );
+var apiRouter       = express.Router();
+var usersController = require( '../controllers/users.js' );
+var User            = require( '../models.user.js' );
 
 
 //ROUTING FUNCTIONS
 //=================
+apiRouter.route( '/users' )
+	.get( usersController.index )
+	.post( usersController.create );
+	
+apiRouter.route( '/users/:user_id' )
+	.get( usersController.show )
+	.patch( usersController.update )
+	.delete( usersController.destroy );
 
 //EXPORT FUNCTIONS
 //================
+module.exports = apiRouter;
